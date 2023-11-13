07635c8a63c06d7f9ef1e4ba8ca8db8069ac6de6
LavakumarPS <lavakumarps7@gmail.com>
2023-11-13 18:53:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function colorChange(){
+    document.body.style.backgroundColor='rgba(' + Math.round(Math.random()*2555) + ',' +  Math.round(Math.random()*255) + ',' +  Math.round(Math.random()*255) +')'
+}
